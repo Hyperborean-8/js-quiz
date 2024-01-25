1a876776dd56a9792a32503cd8ad907f21a9f335
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-01-25 22:20:28 +0300

    Выбор ответа теперь работает (визуально)

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let jsonLength = null;
 let data = null;
 let index = 0;
 let answerBtnArray = [];
+let correctAnswerBtn;
 
 // Переменная пуска, запускается кнопками сложности
 async function start() {
@@ -20,7 +21,7 @@ async function start() {
   console.log(`JSON file contains ${data.questions.length} amount of questions.`);
 
   // Выбор случайного вопроса
-  index = random(0, data.questions.length - 1)
+  index = random(0, data.questions.length - 1);
 
   // prettier-ignore
   console.log(`Random selected the index ${index} from the maximum index ${data.questions.length-1}.`);
@@ -36,27 +37,72 @@ async function start() {
   questionLabel.textContent = data.questions[index]["question"];
 
   // Перемешанный массив со списком ответов
-  let answersArray = shuffle(data.questions[index].answers)
+  let answersArray = shuffle(data.questions[index].answers);
 
   // Создание кнопок с ответами
   for (i = 0; i < answersArray.length; i++) {
+    // Создать кнопку
     answerBtnArray[i] = document.createElement("button");
     answerBtnArray[i].type = "button";
 
-    answerBtnArray[i].innerHTML = answersArray[i];
+    // Подписать кнопку
+    answerBtnArray[i].textContent = answersArray[i];
+
+    // Если кнопка является верным ответом, то добавить её в переменную
+    if (answersArray[i] === data.questions[index]["correctAnswer"]) {
+      console.log(`Правильный ответ записан в кнопку ${answerBtnArray[i]}`);
+      correctAnswerBtn = answerBtnArray[i];
+    }
 
     answerBtnArray[i].classList.add("transparent");
 
     fadeIn(answerBtnArray[i], 1, i + 1);
 
-    // button.onclick = function () {
-    //   // Your code here
-    // };
+    answerBtnArray[i].onclick = function () {
+      checkAnswer(this.textContent, this);
+    };
 
     answersBtnContainer.appendChild(answerBtnArray[i]);
   }
 
   await fadeIn(questionContainer, 1);
+}
+
+function fadeInAllButtons() {}
+
+// При нажатии на кнопку с ответом
+function checkAnswer(answer, button) {
+  if (button === correctAnswerBtn) {
+    console.log(`${answer} is a right answer!`);
+
+    // Подсветить правильный ответ в зелёный цвет
+    button.classList.add("correct");
+
+    // Работа с остальными кнопками
+    answerBtnArray.forEach((element) => {
+      // Затмить кнопки не ответы
+      if (element !== button) {
+        element.classList.add("dismiss");
+      }
+    });
+  } else {
+    console.log(
+      `${answer} is not a right answer! The right answer is ${data.questions[index]["correctAnswer"]}`
+    );
+
+    // Подсветить неправильный ответ в красный цвет
+    button.classList.add("incorrect");
+
+    answerBtnArray.forEach((element) => {
+      // Подсветить правильный ответ в зелёный цвет
+      if (element === correctAnswerBtn) {
+        element.classList.add("correct");
+      } else if (element !== button) {
+        // Затмить каждую кнопку
+        element.classList.add("dismiss");
+      }
+    });
+  }
 }
 
 async function fadeOut(element, time, delay = 0) {
@@ -90,25 +136,25 @@ async function fetchJSON() {
   }
 }
 
-// Функция сна, используется с await (ожидание ответа) 
+// Функция сна, используется с await (ожидание ответа)
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Функция рандома
-function random(min, max){
+function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Функция перемешки массива
 function shuffle(array) {
   let unorderedArray = [];
-   
-  for(let i = array.length - 1; i >= 0; i--) {
-     let randomIndex = Math.floor(Math.random() * (i + 1));
-     let selectedItem = array.splice(randomIndex, 1)[0];
-     unorderedArray.push(selectedItem);
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    let selectedItem = array.splice(randomIndex, 1)[0];
+    unorderedArray.push(selectedItem);
   }
- 
+
   return unorderedArray;
- }
+}
